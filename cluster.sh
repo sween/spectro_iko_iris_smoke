ee7d222f65c11d17a2a79a1dcddb48203df25aa1
Ron Sweeney <ron.sweeney@integrationrequired.com>
2025-07-13 17:45:29 -0400

    Add packaged chart

---
+++ cluster.sh
@@ -0,0 +1,19 @@
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+name: ikosql
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+  - role: control-plane
+  - role: worker
+networking:
+  disableDefaultCNI: true
+EOF
+cilium install --version v1.16.0
+cilium status --wait
+
+helm repo add ikosql https://sween.github.io/spectro_iko_iris_smoke
+helm repo update
+helm search repo ikosql -l
+
+helm install release2 ikosql/ikosql
+
--- gar.sh
+++
@@ -1,8 +0,0 @@
-mkdir gh-pages && cd gh-pages
-git init
-helm repo index . --url https://sween.github.io/spectro_iko_iris_smoke
-git add index.yaml
-git commit -m "Add Helm repo index"
-git branch -M gh-pages
-git remote add origin https://github.com/sween/spectro_iko_iris_smoke.git
-git push -u origin gh-pages
